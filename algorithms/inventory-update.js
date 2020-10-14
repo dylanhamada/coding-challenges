6d6a04d62d02fc89686946852384ceb70b9f15d9
dylanhamada <dylan.hamada@gmail.com>
2020-10-13 20:08:40 -0400

    Finish Writing Inventory Update Algorithm

--- algorithms/inventory-update.js
+++ algorithms/inventory-update.js
@@ -7,17 +7,35 @@ order by item. */
 
 // SOLUTION
 
-/* Iterate through array 2, each time iterating through array 1 to check if the second element in the 
-nested array of array 2 matches the one in array 1. Then...? */
+/* Iterate through array 2, with each iteration also iterating through array 1 to check if the second element 
+in the nested array of array 2 matches the one in array 1. Then...? */
 
 const updateInventory = (arr1, arr2) => {
     arr2.forEach(arrTwoEl => {
+        let newItem = false;
+
         arr1.forEach(arrOneEl => {
             if (arrOneEl[1] === arrTwoEl[1]) {
-
+                arrOneEl += arrTwoEl[0];
+                newItem = true;
             }
         });
+
+        if (!newItem) {
+            arr1.push(arrTwoEl);
+        }
     });
+
+    arr1.sort((a, b) => {
+        if (a[1] < b[1]) {
+            return -1;
+        } else if (a[1] < b[1]) {
+            return 1;
+        }
+        return 0;
+    });
+
+    console.log(arr1);
 };
 
 var curInv = [
