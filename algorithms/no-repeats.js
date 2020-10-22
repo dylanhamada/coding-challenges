693795850ff9a8185046ced91f39a0305477ab84
dylanhamada <dylan.hamada@gmail.com>
2020-10-21 21:01:20 -0400

    Begin Recursive Solution to No Repeats

--- algorithms/no-repeats.js
+++ algorithms/no-repeats.js
@@ -5,16 +5,40 @@ Assume that all characters in the provided string are each unique. */
 
 // SOLUTION
 
+/*
+PLAIN LANGUAGE
+
+- recursive function
+- if current character index equals the length of the string (minus 1), store the string (in an array) and return
+- otherwise, using a for loop, swap the character with itself and each subsequent character
+- store the resulting string (in the array), move the character index forward 1, and call the recursive function on this resulting string
+*/
+
 const permAlone = str => {
-    /*
-    PLAIN LANG
+    let permutations = [];
 
-    - define a swap function
-    - create a for loop that starts at 0, and runs x times, where s is the number of characters
-    - each time the loop runs, it swaps the first char with the next one in sequence
-    - however, the list of strings that results from this loop also has to run the swap func
-    - but each successive run, the first x chars of the string are frozen
+    const swapFunc = (arr, indOne, indTwo) => {
+        let newArr = [...arr];
+        let temp = newArr[indOne];
 
-    - is there a way to do this recursively? what condition would we look for to terminate the recursion?
-    */
+        newArr[indOne] = newArr[indTwo];
+        newArr[indTwo] = temp;
+
+        return newArr;
+    };
+
+    const recursiveFunc = (str, ind) => {
+        let strArr = str.split('');
+
+        if (ind === (strArr.length - 1)) {
+            permutations.push(strArr);
+            return;
+        }
+
+        for (let i = ind; i < strArr.length; i++) {
+            let newArr = swapFunc(strArr, ind, i);
+            permutations.push(newArr);
+            
+        }
+    };
 };
