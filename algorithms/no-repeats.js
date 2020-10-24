66be0ca2ad76bf2ce302741fb9e5fbd7072ab376
dylanhamada <dylan.hamada@gmail.com>
2020-10-23 20:50:13 -0400

    Solve Permutations Problem in No Repeats

--- algorithms/no-repeats.js
+++ algorithms/no-repeats.js
@@ -5,40 +5,50 @@ Assume that all characters in the provided string are each unique. */
 
 // SOLUTION
 
-/*
-PLAIN LANGUAGE
-
-- recursive function
-- if current character index equals the length of the string (minus 1), store the string (in an array) and return
-- otherwise, using a for loop, swap the character with itself and each subsequent character
-- store the resulting string (in the array), move the character index forward 1, and call the recursive function on this resulting string
-*/
-
 const permAlone = str => {
+    // Array to store all unique permutations of string
     let permutations = [];
 
+    // Swap two elements in an array
     const swapFunc = (arr, indOne, indTwo) => {
         let newArr = [...arr];
+        // Temporarily store the element at index indOne
         let temp = newArr[indOne];
 
+        // Overwrite the element at index indOne with the element at indTwo
         newArr[indOne] = newArr[indTwo];
+        // Overwrite the element at index indTwo with the temporarily stored element
         newArr[indTwo] = temp;
 
         return newArr;
     };
 
-    const recursiveFunc = (str, ind) => {
+    // Recursively find all permutations
+    const recursivePermute = (str, ind) => {
         let strArr = str.split('');
 
+        // If index is last position in string, push the permutation to the array and return
         if (ind === (strArr.length - 1)) {
-            permutations.push(strArr);
+            permutations.push(str);
             return;
         }
 
+        /* Starting with the current index, swap with each subsequent character position, then call this
+        function recursively, increasing the index by one */
         for (let i = ind; i < strArr.length; i++) {
             let newArr = swapFunc(strArr, ind, i);
-            permutations.push(newArr);
-            
+            let newStr = newArr.join('');
+            if (permutations.includes(newStr)) {
+                permutations.push(newStr);
+            }
+            recursivePermute(newStr, ind + 1);
         }
     };
+
+    // Make initial call to recursive function, passing the string and index zero
+    recursivePermute(str, 0);
+
+    return permutations;
 };
+
+console.log(permAlone("abcd"));
